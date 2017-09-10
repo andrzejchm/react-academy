f535c484bd5b18da78a1cef780c145900872c76d
Andrzej Chmielewski <andrzejchm@gmail.com>
2017-09-10 13:30:15 +0200

    add config and strings files

---
+++ src/config/config.js
@@ -0,0 +1,22 @@
+export default {
+  routes: {
+    repairs: {
+      path: '/repairs',
+      activeTab: 1,
+    },
+    repairDetails: {
+      path: '/repairs/:id',
+      activeTab: 1,
+    },
+    users: {
+      path: '/users',
+      activeTab: 2,
+    },
+    login: {
+      path: '/login',
+    },
+    register: {
+      path: '/register',
+    },
+  },
+};
---
+++ src/config/strings.js
@@ -0,0 +1,13 @@
+export default {
+  app_name: 'Car repair',
+  login_title: 'Login',
+  register_title: 'Register',
+  username_hint: 'Your username',
+  password_hint: 'password',
+  confirm_password_hint: 'confirm password',
+  dont_have_account_question: 'Don\'t have an account?',
+  already_have_account_question: 'Already have an account?',
+  login_button: 'Login',
+  register_button: 'Register',
+
+};
