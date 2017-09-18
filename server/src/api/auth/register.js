a5d00e0e37fbd68a6df875626f55ddffb2d37279
Andrzej Chmielewski <andrzejchm@gmail.com>
2017-09-18 16:57:31 +0200

    add eslint to server

--- server/src/api/auth/register.js
+++ server/src/api/auth/register.js
@@ -6,7 +6,7 @@ import errorCodes from '../../config/errorCodes';
 export default function register(req, res) {
   if (users.getUser(req.body.username)) {
     res.status(400);
-    res.json(wrap(null, 'user already exists',errorCodes.user_already_exists));
+    res.json(wrap(null, 'user already exists', errorCodes.user_already_exists));
   } else {
     users.addUser({
       username: req.body.username,
