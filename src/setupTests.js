d828c34e1f2539e3305a893064c08a569f0032e1
Andrzej Chmielewski <andrzejchm@gmail.com>
2017-09-20 16:56:22 +0200

    fix eslint

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,10 +1,6 @@
 /* eslint-disable import/no-extraneous-dependencies */
-import { initialState as userInfoInitialState } from './redux/reducers/userInfoReducer';
 import { configureStore } from './redux/store';
 /* eslint-enable */
-const initialState = {
-  userInfo: userInfoInitialState,
-};
 
 export default function configureTestStore() {
   return configureStore();
